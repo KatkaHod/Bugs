3e9a78f51ca54a2e3b50301a7f0285e4a13e8198
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-01-23 22:41:17 +0100

    All method from the Bugs class called.

--- src/packageBugs/Bugs.java
+++ src/packageBugs/Bugs.java
@@ -4,28 +4,27 @@ import java.math.BigDecimal;
 
 public class Bugs {
 
-
-    public static void ukol1() {
+    public static void homework1() {
             String pozdrav = "Hello World!";
              System.out.println(pozdrav);
         }
 
-        public static void ukol2() {
+        public static void homework2() {
             String uzivatel = "Karel";
              System.out.println(uzivatel);
         }
 
-        public static void ukol3() {
+        public static void homework3() {
             int pocetLekci = 10;
              System.out.println(pocetLekci);
         }
 
-        public static void ukol4() {
+        public static void homework4() {
          double cena = 10.0;
           System.out.println(cena);
         }
 
-        public static void ukol6() {
+        public static void homework5() {
             BigDecimal cena = BigDecimal.valueOf(0.0);
             cena = cena.add(BigDecimal.valueOf(0.1));
 
@@ -33,7 +32,7 @@ public class Bugs {
         }
 
 
-        public static void ukol5() {
+        public static void homework6() {
             BigDecimal bdcena = BigDecimal.valueOf(0.0);
             BigDecimal bdzvysenihodnoty = BigDecimal.valueOf(0.1);
 
@@ -43,19 +42,17 @@ public class Bugs {
             System.out.println(bdcena);
         }
 
-
-
-        public static void ukol7() {
+        public static void homework7() {
             int vykonMotoru = 120;
             System.out.println("Výkon Motoru je: " + vykonMotoru + " kW.");
         }
 
-        public static void ukol8() {
+        public static void homework8() {
             int velikostKosile = 37;
             System.out.println("Velikost košile je: " + velikostKosile + ".");
         }
 
-        public static void ukol9() {
+        public static void homework9() {
             System.out.println("Metoda *skoro* bez chybičky!");
         }
 
--- src/packageBugs/homeworksPrint.java
+++ src/packageBugs/homeworksPrint.java
@@ -3,7 +3,15 @@ package packageBugs;
 public class homeworksPrint {
     public static void main(String[] args) {
         Bugs bugs = new Bugs();
+        System.out.println("Methods call from the Bugs class");
 
+       System.out.print("1.: ");      bugs.homework1();
+       System.out.print("2.: ");      bugs.homework2();
+       System.out.print("3.: ");      bugs.homework3();
+       System.out.print("4.: ");      bugs.homework4();
+       System.out.print("5.: ");      bugs.homework5();
+       System.out.print("6.: ");      bugs.homework6();
+       System.out.print("7.: ");      bugs.homework7();
 
 
     }
